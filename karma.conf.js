de976ed5b84380570691d1589953acae64aac935
rodrigoafb <rodrigo.afb@hotmail.com>
2015-05-11 11:33:25 -0300

    MapChart

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,10 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'app/*.js',
+      'app/factory/*.js',
+      'app/charts/*.js',
+      'test/*.js'
     ],
 
 
--- test/mapChartTestSpec.js
+++ test/mapChartTestSpec.js
@@ -1,6 +1,10 @@
 /// <reference path="../typings/jasmine/jasmine.d.ts"/>
 describe('Map Chart - ', function name() {
 	
-	it('');
+	it('Must draw chart', function name() {
+		
+		var mapChart = new chart.charts.Map()
+		
+	});
 	
 });
