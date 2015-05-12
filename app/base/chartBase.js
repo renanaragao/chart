b2c74d9a24cb0b6555a77d6b8f1904cbe0909d58
Renan Aragão <renanferreiradearagao@yahoo.com.br>
2015-05-12 11:02:36 -0300

    Finishied ChartBase. Event Click and create datatable chart.

--- app/base/chartBase.js
+++ app/base/chartBase.js
@@ -12,23 +12,145 @@
 		var settings = {
             source: '',
             data: null,
+            responsive: false,
             options: {
                 height: 100,
                 width: 100
             },
-            selected: function (pchart) { },
+            select: function (pchart) { },
             mouseOver: function (pchart) { }
         };
         
         $.extend(true, settings, options);
+        
+        self._draw = draw;
         
         init();
         
         function init() {
             
             validate();
+            window.google.setOnLoadCallback(drawChart);
             
-            //google.setOnLoadCallback(drawChart());
+        }
+        
+        function drawChart() {
+            
+            var dataTable = getDataTable(settings.data);
+            
+            var data = window.google.visualization.arrayToDataTable(dataTable);
+            
+            if(settings.responsive) settings.options.width = data.getNumberOfColumns() * 65;
+            
+            var chart = self._draw(el, data, settings.options);
+            
+            window.google.visualization.events.addListener(chart, 'select', settings.select);
+            
+        }
+        
+        function getDataTable(data) {
+            
+            var dataTable = [];
+            var columns = getColumns(data);
+            var rows = getRows(columns, data);
+            
+            dataTable.push(columns);
+
+            var countRows = rows.length;
+
+            for (var i = 0; i < countRows; i++) {
+                dataTable.push(rows[i]);
+            }
+            
+            return dataTable;
+            
+        }
+        
+        function getColumns(data) {
+            
+            var count = data.length;
+            var columns = ['Grupo'];
+
+            var groups = [],
+                found;
+
+            for (var i = 0; i < count; i++) {
+
+                found = groups.filter(function (item) {
+                    return item === data[i]["Grupo"];
+                });
+
+                if (found.length === 0) {
+                    groups.push(settings.data[i]["Grupo"]);
+                }
+            }
+
+            columns.push.apply(columns, groups);
+
+            return columns;
+            
+        }
+        
+        function getRows(columns, data) {
+            
+            var count = data.length;
+
+            var rows = [], found;
+
+            var groups = [];
+
+            for (var i = 0; i < count; i++) {
+
+                found = groups.filter(function (item) {
+                    return item === settings.data[i]["Descricao"];
+                });
+
+                if (found.length === 0) {
+                    groups.push(settings.data[i]["Descricao"]);
+                }
+            }
+
+            count = groups.length;
+
+            for (var y = 0; y < count; y++) {
+
+                var row = [];
+
+                row.push(groups[y]);
+
+                var countColumns = columns.length;
+
+                for (var j = 1; j < countColumns; j++) {
+
+                    row.push(getValues(groups[y], columns[j], data));
+
+                }
+
+                rows.push(row);
+            }
+            
+            return rows;
+            
+        }
+        
+        function getValues(row, column, data) {
+            
+            var found = data.filter(function (item) {
+
+                return item["Descricao"] === row && item["Grupo"] === column;
+
+            });
+
+            if (found.length === 0) return { v: 0, f: '0' };
+
+            var value = found.reduce(function (ant, current) {
+
+                return { Valor: ant["Valor"] + current["Valor"] };
+
+            }).Valor;
+
+
+            return { v: value, f: (value).toFixed(2) };
             
         }
         
@@ -40,6 +162,10 @@
             
         }
         
+        function draw() {
+            
+        }
+        
 	};
 	
-})(chart);
+})(window.chart);
--- test/chartBaseTestSpec.js
+++ test/chartBaseTestSpec.js
@@ -3,11 +3,34 @@ describe('ChartBase - ', function () {
 	
 	'use strict';
 	
-	var callbackExpected;
+	var callbackExpected,
+		data = [
+            { Grupo: 'sp', Descricao: 'a', Valor: 10 },
+            { Grupo: 'sp', Descricao: 'b', Valor: 5 },
+            { Grupo: 'sp', Descricao: 'c', Valor: 2 },
+            { Grupo: 'rj', Descricao: 'b', Valor: 4 },
+            { Grupo: 'rj', Descricao: 'c', Valor: 6 },
+            { Grupo: 'sc', Descricao: 'a', Valor: 8 },
+            { Grupo: 'sc', Descricao: 'c', Valor: 10 },
+            { Grupo: 'bh', Descricao: 'c', Valor: 20 },
+            { Grupo: 'bh', Descricao: 'b', Valor: 20 }
+    ],
+	dataTable = [];
 	
 	var google = {
 		setOnLoadCallback: function (callback) {
 			callbackExpected = callback;
+		},
+		visualization: {
+			arrayToDataTable: function (data) {
+				dataTable = data;
+				
+				return data;
+			},
+			events: {
+						addListener: function (chart, event, select) {
+						}
+					}
 		}
 	};
 	
@@ -24,6 +47,8 @@ describe('ChartBase - ', function () {
 			new chart.charts.ChartBase();
 			
 		}).toThrow(new Error('Data source not found.'));
+		
+		window.google = google;
 		
 		new chart.charts.ChartBase({source: 'url'});
 		
@@ -43,11 +68,188 @@ describe('ChartBase - ', function () {
 		
 	});
 	
-	xit("Must run the google's mehtod setOnLoadCallback", function () {
+	it("Must run google's method setOnLoadCallback", function () {
+		
+		window.google = google;
 		
 		new chart.charts.ChartBase({data: []});
 		
 		expect(typeof callbackExpected).toEqual('function');
+		
+	});
+	
+	it("Must run google's method visualization.arrayToDataTable", function () {
+		
+		window.google = google;
+		
+		new chart.charts.ChartBase({data: data});
+		
+		var dataTableExpected = [
+			['Grupo', 'sp', 'rj', 'sc', 'bh'],
+			['a', {v: 10, f: '10.00'}, {v: 0, f: '0'}, {v: 8, f: '8.00'}, {v: 0, f: '0'}],
+			['b', {v: 5, f: '5.00'}, {v: 4, f: '4.00'}, {v: 0, f: '0'}, {v: 20, f: '20.00'}],
+			['c', {v: 2, f: '2.00'}, {v: 6, f: '6.00'}, {v: 10, f: '10.00'}, {v: 20, f: '20.00'}],
+		];
+		
+		callbackExpected();
+		
+		expect(dataTable).toEqual(dataTableExpected);
+		
+	});
+	
+	it('Must draw chart', function () {
+		
+		window.google = google;
+		
+			var elementExpected = {},
+				optionsExpected = {},
+				dataTableExpected = {};
+		
+		var ChartFake = function (options, el) {
+			
+			var self = this;
+			
+			chart.charts.ChartBase.call(self, options, el);
+			
+			self._draw = draw;
+			
+			function draw(pElement, datatable, pOptions) {
+				elementExpected = pElement;
+				optionsExpected = pOptions;
+				dataTableExpected = datatable;
+			}
+		}
+		
+		ChartFake.prototype = Object.create(chart.charts.ChartBase.prototype);
+		
+		var options = {data: data, options: {width: 15}},
+			el = {node: 'div'},
+		    dataTable = [
+			['Grupo', 'sp', 'rj', 'sc', 'bh'],
+			['a', {v: 10, f: '10.00'}, {v: 0, f: '0'}, {v: 8, f: '8.00'}, {v: 0, f: '0'}],
+			['b', {v: 5, f: '5.00'}, {v: 4, f: '4.00'}, {v: 0, f: '0'}, {v: 20, f: '20.00'}],
+			['c', {v: 2, f: '2.00'}, {v: 6, f: '6.00'}, {v: 10, f: '10.00'}, {v: 20, f: '20.00'}],
+		];;
+		
+		new ChartFake(options, el);
+		
+		callbackExpected();
+		
+		expect(elementExpected).toEqual(el);
+		expect(optionsExpected).toEqual({width: 15, height: 100});
+		expect(dataTableExpected).toEqual(dataTable);
+		
+	});
+	
+	it("If options's property 'responsive' to equal true, must calculate width based in number of columns", function () {
+		
+		var optionsExpected = {
+			width: 23
+		};
+		
+		window.google = {
+				setOnLoadCallback: function (callback) {
+					callbackExpected = callback;
+				},
+				visualization: {
+				arrayToDataTable: function (data) {
+					dataTable = data;
+					
+					return {
+						getNumberOfColumns: function () {
+							return 2;
+						}
+					};
+				},
+				events: {
+						addListener: function (chart, event, select) {
+						}
+					}
+			}
+		};
+		
+		var ChartFake = function (options, el) {
+			
+			var self = this;
+			
+			chart.charts.ChartBase.call(self, options, el);
+			
+			self._draw = draw;
+			
+			function draw(pElement, datatable, pOptions) {
+				optionsExpected = pOptions;
+			}
+		}
+		
+		ChartFake.prototype = Object.create(chart.charts.ChartBase.prototype);
+		
+		new ChartFake({data: data, options: {width: 23}});
+		
+		callbackExpected();
+		
+		expect(optionsExpected).toEqual({width: 23, height: 100});
+		
+		new ChartFake({data: data, options: {width: 23}, responsive: true});
+		
+		callbackExpected();
+		
+		expect(optionsExpected).toEqual({width: 130, height: 100});
+		
+	});
+	
+	it('Must capture the event click of mouse', function () {
+		
+		var eventEspected = {};
+		
+		var selectedChart = false;
+		
+		window.google = {
+				setOnLoadCallback: function (callback) {
+						callbackExpected = callback;
+					},
+				visualization: {
+					arrayToDataTable: function (data) {
+						},
+					events: {
+						addListener: function (chart, event, select) {
+							eventEspected.chart = chart;
+							eventEspected.event = event;
+							eventEspected.select = select;
+						}
+					}
+				}
+		};
+		
+		var ChartFake = function (options, el) {
+			
+			var self = this;
+			
+			chart.charts.ChartBase.call(self, options, el);
+			
+			self._draw = draw;
+			
+			function draw(pElement, datatable, pOptions) {
+				return 'chart';
+			}
+		}
+		
+		ChartFake.prototype = Object.create(chart.charts.ChartBase.prototype);
+		
+		new ChartFake({
+				data: [],  
+				responsive: false,
+				select: function (chart) {
+					selectedChart = true;
+				}
+			});
+		
+		callbackExpected();
+		
+		expect(eventEspected.chart).toEqual('chart');
+		expect(eventEspected.event).toEqual('select');
+		
+		eventEspected.select();
+		expect(selectedChart).toEqual(true);
 		
 	});
 	
